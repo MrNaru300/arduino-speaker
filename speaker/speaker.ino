6308d6a1db608041be7d941382ba22d864bab1e9
MrNaru300 <bruno.santi.oli@gmail.com>
2020-12-15 15:39:18 -0300

    music working almost flawless

--- speaker/speaker.ino
+++ speaker/speaker.ino
@@ -1,15 +1,16 @@
 #define RATE 14000 //It need to be less than or equal to BAUD_RATE/BAUD_RATE
-#define CHUNK 8
+#define CHUNK 16
 #define BAUD_RATE 115200 //Using maximum baud rate for more audio quality
 
 byte buf[CHUNK];
+const unsigned long period = 1000000/RATE;
 
 void setup() {
   for (int pin = 2; pin < 9; pin++) {
   	pinMode(pin, OUTPUT);
     digitalWrite(pin, LOW);
   }
-  //testWaves(1440, 1);
+  testWaves(1440, 1);
   Serial.begin(BAUD_RATE);
   Serial.println("");
 }
@@ -19,15 +20,15 @@ void loop() {
   playMusic();
 }
 
-void testWaves(int freq, float time) {
+void testWaves(int freq, float t) {
   //Tests every wave tone for especific time 
-  for (int i = 0; i < time*freq; i++) {
+  for (int i = 0; i < t*freq; i++) {
    squareTone(freq); 
   }
-  for (int i = 0; i < time*freq; i++) {
+  for (int i = 0; i < t*freq; i++) {
    sawtoothTone(freq); 
   }
-  for (int i = 0; i < time*freq; i++) {
+  for (int i = 0; i < t*freq; i++) {
    sineTone(freq); 
   }
 }
@@ -36,7 +37,7 @@ void sawtoothTone(int freq) {
   //Play one wave legth of a sawtooth tone
   for (int i = 0; i < RATE/freq; i++) {
  	playSample(i);
-   	delayMicroseconds(1000000/RATE);
+   	delayMicroseconds(period);
  }
  playSample(0);
 }
@@ -44,9 +45,9 @@ void sawtoothTone(int freq) {
 void squareTone(int freq) {
   //Play one wave legth of a square tone
   playSample(255);
-  delayMicroseconds(floor(500000/freq));
+  delayMicroseconds(period/2);
   playSample(0);
-  delayMicroseconds(floor(500000/freq));
+  delayMicroseconds(period/2);
 }
 
 void sineTone (int freq) {
@@ -55,7 +56,7 @@ void sineTone (int freq) {
   for (int i = 0; i < RATE/freq; i++) {
     pos = sin(6.28318530718*i*freq/RATE);
     playSample(floor((pos+1)*127));
-    delayMicroseconds(floor(1000000/RATE));
+    delayMicroseconds(period);
   }
   playSample(0);
 }
@@ -64,8 +65,8 @@ void sineTone (int freq) {
 void playMusic() {
   //Automatically play every sample from buffer
   for (int i = 0; i < CHUNK; i++) {
-    playSample(buf[i]);
-    delayMicroseconds(floor(1000000/RATE));
+    playSample(map(buf[i], 0, 255, 192, 255));
+    delayMicroseconds(period);
   }
 }
 
